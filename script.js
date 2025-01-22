f9e7f26a917acb06ed8b1ff9c1559f7f9a250fb3
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-22 16:40:24 +0100

    [console.log ciclo ]

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // Attraverso l’apposita API di Boolean
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
-// setto la variabile
+// elemento di output
 const randomEmail = document.getElementById('result')
 
 //creo array vuoto per contenere l email generate
 let gMail = [];
 
 // creo ciclo per le 10 random email
-for ( let i = 0; i < 10; i++){
+for ( let i = 0; i <= 10; i++){
     // chiamata API per generare un email
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => {
-        console.log(response)
+        //codice da eseguire in caso di successo
+        const risultato = response.data.response
+
+
+        gMail.push(risultato);
+
+        console.log(risultato)
+
+        
     })
 }
+
+
